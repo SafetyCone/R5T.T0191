8dc3d167046efa7fc83721f21b95348c284b29bf
davidcoats <david@getsafetycone.com>
2023-07-27 01:32:22 -0700

    Interim changes.

--- source/R5T.T0191/Code/Extensions/StringExtensions.cs
+++ source/R5T.T0191/Code/Extensions/StringExtensions.cs
@@ -5,11 +5,13 @@ namespace R5T.T0191.Extensions
 {
     public static class StringExtensions
     {
+        /// <inheritdoc cref="IStringOperator.ToDataDirectoryPath(string)"/>
         public static IDataDirectoryPath ToDataDirectoryPath(this string value)
         {
             return Instances.StringOperator_Extensions.ToDataDirectoryPath(value);
         }
 
+        /// <inheritdoc cref="IStringOperator.ToDocumentsDirectoryPath(string)"/>
         public static IDocumentsDirectoryPath ToDocumentsDirectoryPath(this string value)
         {
             return Instances.StringOperator_Extensions.ToDocumentsDirectoryPath(value);
--- source/R5T.T0191/Code/Functionality/IStringOperator-Extensions.cs
+++ source/R5T.T0191/Code/Functionality/IStringOperator-Extensions.cs
@@ -8,12 +8,14 @@ namespace R5T.T0191.Extensions
     [FunctionalityMarker]
     public partial interface IStringOperator : IFunctionalityMarker
     {
+        /// <inheritdoc cref="IDataDirectoryPath"/>
         public IDataDirectoryPath ToDataDirectoryPath(string value)
         {
             var output = new DataDirectoryPath(value);
             return output;
         }
 
+        /// <inheritdoc cref="IDocumentsDirectoryPath"/>
         public IDocumentsDirectoryPath ToDocumentsDirectoryPath(string value)
         {
             var output = new DocumentsDirectoryPath(value);
